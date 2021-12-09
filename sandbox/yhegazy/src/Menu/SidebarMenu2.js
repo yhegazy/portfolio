87a3a2502ed7fe04fd15c79db889110225c49fdd
yhegazy <hegazyx@gmail.com>
2021-12-09 08:01:38 -0500

    20211209::Reverted to original SBM2 and copied 2nd  push SMB2 and created SMB3

--- sandbox/yhegazy/src/Menu/SidebarMenu2.js
+++ sandbox/yhegazy/src/Menu/SidebarMenu2.js
@@ -1,59 +1,59 @@
 import {useEffect, useState} from 'react'
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
 import menuItems from '../Menu/MenuItems2'
+import MenuItems from '../Menu/MenuItems2'
 
 
+const sidebarComposition = "fixed top-0 left-0 h-full transition-all duration-500"
+const menuComposition = ""
+const ToggleCSS = 'text-3xl text-white text-center leading-normal px-4'
+const iconComposition = "text-2xl text-center leading-normal mx-4"
+const spanComposition = "text-xl font-medium text-center align-baseline "
+// absolute left-full -top-3 px-4 py-5 opacity-0 block pointer-events-none hover:top-0 hover:opacity-100 hover:pointer-events-auto hover:transition-all hover:duration-500 hover:ease-linear
 
-// TODO Today:: This is all under something called a MenuToggle. There will be 2 builds, 1 open & 1 closed. Today, I will only focus on the open aspect of this build. In Sha Allah, I will succeed.  - DONE!
-const SIDEBAR_ICON = 'text-3xl text-white text-center leading-normal px-4'
-const SIDEBAR_LOGO = "text-2xl font-semibold text-white transition duration-300 delay-100 ease"
-
-// "absolute -top-1.5 mt-0 pt-1.5 pl-20 rounded-md block pointer-events-none opacity-20 bg-green-400 "
 
 
 
 const SidebarMenu2 = () => {
     const [menuToggle, setMenuToggle] = useState(false)
-    
+    const [subToggle, setSubToggle] = useState(false)
+   
+
     return <>
-        <nav className={`SIDEBAR ${menuToggle ? "SIDEBAR_OPEN" : "SIDEBAR_CLOSED"}`}>
-            <div className={menuToggle && "flex items-center w-full h-auto"}>
+        <nav className={`${sidebarComposition} ${menuToggle ? 'w-64  bg-indigo-900 bg-opacity-50': 'w-16 '}`}>
+            <div>
                 <i 
-                    className={` ${SIDEBAR_ICON} ${menuToggle ? "bx bx-x" : "bx bx-menu" }`} 
+                    className={` ${ToggleCSS} ${menuToggle ? "bx bx-x" : "bx bx-menu" }`} 
                     onClick={() => setMenuToggle(!menuToggle)}>
                 </i>
-                <p className={SIDEBAR_LOGO}>LOGONAME</p>
-                
             </div>
-
-            <ul className={menuToggle && "SUBMENU_UL"}>
-                {menuItems.map((item) => 
-                    <li key={item.id} className={menuToggle && "SUBMENU_LI"}>
-                        <div className={menuToggle}>
-                            <p className="flex items-center no-underline">
-                                <i className={`SUBMENU_LI_I ${item.icon}`} ></i>
-                                
-                                <span className={menuToggle && "text-lg text-white transition-all duration-500 ease-linear"}>{item.title}</span>
-                            </p>
-                        </div>
-            
-                        {/* sub-menu style.css */}
-                        <ul className={menuToggle && "py-0.5 pr-1.5 -mt-2.5" }>
-                        
-                            <li className={menuToggle && "block"}>
-                               {!menuToggle && <p className="SUBMENU_A">{item.title}</p>}
-                            </li>
-                            {item.subMenus ? item.subMenus.map((subItem, index) => {
+            <ul className={menuToggle ? "text-white" : "bg-indigo-50"}>
+                {menuItems.map((item) => {
+                    return <>
+                        <li className="my-6">
+                            <div className="space-x-2"> 
+                                <i className={`${iconComposition} ${item.icon}`}></i>
+                                <span className={spanComposition}>{menuToggle ? item.title : ''}</span>
+                                {item.subMenus ? //when clicking on the down arrow, it automatically closes the previous DD. Ignore the animation
+                                    <i id={item.id} className="px-5 bx bx-caret-down"></i>: null}
+                            </div>
+                            {item.subMenus ? !subToggle && item.subMenus.map((subItem) => {
                                 return <>
-                                    <li id={index} className={menuToggle ? "w-1/2 pb-1 ml-auto mr-auto": ""}>
-                                        <p className="py-0.5 text-white transition-all duration-300 ease-linear opacity-50 whitespace-nowrap hover:opacity-70">{subItem.title}</p>
-                                    </li>
+                                    <ul>
+                                        <li>
+                                            <div>
+                                                <i className={`${iconComposition} ${subItem.icon}`}></i>
+                                                <span className={spanComposition}>{menuToggle ? subItem.title : ''}</span>
+                                            </div>
+                                        </li>
+                                    </ul>
                                 </>
-                            })
-                            : ""}
-                        </ul>
-                    </li>
-                )}
+                            }) 
+                            :
+                                null}
+                        </li>
+                    </>
+                })}
                 
             </ul>
         </nav>
--- sandbox/yhegazy/src/Menu/SidebarMenu3.js
+++ sandbox/yhegazy/src/Menu/SidebarMenu3.js
@@ -1,103 +1,111 @@
-import React, {useState} from 'react'
-
-export const menuData = [
-    {
-        id: 1,
-        title: 'Home',
-        exact: true,
-        to: '/',
-        icon: 'bx bx-home-alt',
-        
-    },
-    {
-        id: 2,
-        title: 'About',
-        exact: true,
-        to: '/about',
-        icon: 'bx bx-font',
-        subMenus: [
-        { title: "Resume", to: "/about/resume" },
-        { title: "Random", to: "/about/random" },
-        ],
-        subIcon: ' bx bx-subdirectory-right'
-    },
-    {
-        id: 3,
-        title: 'Sandbox',
-        exact: true,
-        to: '/sandbox',
-        icon: 'bx bx-bracket ',
-        subMenus: [
-        { title: "Web Dev", to: "/skeletal" }, 
-        { title: "Web Scrape", to: "/webscrape" },
-        ],
-    },
-    {
-        id: 4,
-        title: 'Contact',
-        exact: true,
-        to: '/contact',
-        icon: 'bx bx-comment-detail',
-    },
-    {
-        id: 5,
-        title: 'User Profile',
-        exact: true,
-        to: '/userprofile',
-        icon: 'bx bx-face',
-    },
-];
-
-const sidebarComposition = "fixed top-0 left-0 h-full transition-all duration-500"
-const menuComposition = ""
-const ToggleCSS = 'text-3xl text-white text-center leading-normal px-4'
-const iconComposition = "text-2xl text-center leading-normal mx-4"
-const spanComposition = "text-xl font-medium text-center align-baseline "
+import {useEffect, useState} from 'react'
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import menuItems from '../Menu/MenuItems2'
 
 
-const SidebarMenu = () => {
-    const [mToggle, setMToggle] = useState(false)
-    const handleMenuItemToggle = () =>  setMToggle(!mToggle)
 
-return <>
+// TODO Today:: This is all under something called a MenuToggle. There will be 2 builds, 1 open & 1 closed. Today, I will only focus on the open aspect of this build. In Sha Allah, I will succeed.  - DONE!
+const SIDEBAR_ICON = 'text-3xl text-white text-center leading-normal px-4'
+const SIDEBAR_LOGO = "text-2xl font-semibold text-white transition duration-300 delay-100 ease"
 
-        <nav className={`sidebar ${mToggle ? "" : "close"}`}>
-            <div className="logo-details">
-                <i 
-                    className={` ${ToggleCSS} ${mToggle ? "bx bx-x" : "bx bx-menu" }`} 
-                    onClick={() => setMToggle(!mToggle)}>
-                </i>
-            </div>
-            <ul className="nav-links">
-                {menuData.map((item) => 
-                    <li key={item.id}>
-                        <div className={mToggle ? "flex item-center justify-between":"block"}>
-                            <p className="flex items-center no-underline">
-                                <i className={item.icon} ></i>
-                                <span className={mToggle ? "text-lg text-white transition-all duration-500 ease-linear ": "opacity-0 pointer-events-none"}>{item.title}</span>
-                            </p>
-                        </div>
-            
-                        {/* sub-menu style.css */}
-                        <ul className={mToggle ? "text-lg text-white opacity-100 border-b-4 border-dotted border-gray-800" : "sub-menu"}>
-                        
-                            <li>
-                               {mToggle ? "" :  <p className="text-lg text-white transition-all duration-500 ease-linear ">{item.title}</p>}
-                            </li>
-                            {item.subMenus ? item.subMenus.map((subItem, index) => {
-                                return <>
-                                    <li id={index} className={mToggle ? "w-1/2 pb-1 ml-auto mr-auto": ""}>
-                                        <p className="py-1 text-white transition-all duration-300 ease-linear whitespace-nowrap opacity-30 hover:opacity-70">{subItem.title}</p>
-                                    </li>
-                                </>
-                            }): ""}
-                        </ul>
-                    </li>
-                )}
+// "absolute -top-1.5 mt-0 pt-1.5 pl-20 rounded-md block pointer-events-none opacity-20 bg-green-400 "
+
+
+
+const SidebarMenu2 = () => {
+    const [menuToggle, setMenuToggle] = useState(false)
+
+    const Open = () => {
+        return <>
+            <nav className={`SIDEBAR SIDEBAR_OPEN`}>
+                <div className="flex items-center w-full h-auto">
+                    <i 
+                        className={` ${SIDEBAR_ICON} bx bx-x`} 
+                        onClick={() => setMenuToggle(!menuToggle)}>
+                    </i>
+                    <p className={SIDEBAR_LOGO}>LOGONAME</p>
+                    
+                </div>
+
+                <ul className="SUBMENU_UL">
+                    {menuItems.map((item) => 
+                        <li key={item.id} className="SUBMENU_LI">
+                            <div>
+                                <p className="flex items-center no-underline">
+                                    <i className={`SUBMENU_LI_I ${item.icon}`} ></i>
+                                    
+                                    <span className="text-lg text-white transition-all duration-500 ease-linear">{item.title}</span>
+                                </p>
+                            </div>
                 
-            </ul>
-        </nav>
+                            {/* sub-menu style.css */}
+                            <ul className="py-0.5 pr-1.5 -mt-2.5">
+                                {item.subMenus ? item.subMenus.map((subItem, index) => {
+                                    return <>
+                                        <li id={index} className="w-1/2 pb-1 ml-auto mr-auto">
+                                            <p className="py-0.5 text-white transition-all duration-300 ease-linear opacity-50 whitespace-nowrap hover:opacity-70">{subItem.title}</p>
+                                        </li>
+                                    </>
+                                })
+                                : ""}
+                            </ul>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </>
+    }
+
+    const Closed = () => {
+        return <> 
+            <nav className={`SIDEBAR SIDEBAR_CLOSED `}>
+                <div>
+                    <i 
+                        className={` ${SIDEBAR_ICON} bx bx-menu`} 
+                        onClick={() => setMenuToggle(!menuToggle)}>
+                    </i>
+                </div>
+
+                <ul className="overflow-visible">
+                    {menuItems.map((item) => 
+                        <li key={item.id} className="block px-5 pt-4 rounded-md opacity-100 pointer-events-none left-full duration-0 delay-0">
+                            <div className="bg-green-500 ">
+                                <p className="flex items-center space-x-3 no-underline">
+                                    <i className={`SUBMENU_LI_I ${item.icon}`} ></i>
+                                    
+                                    <span className="">{item.title}</span>
+                                </p>
+                            </div>
+                
+                            {/* sub-menu style.css */}
+                            <ul>
+                            
+                                <li className="SUBMENU_A">
+                                {item.title}
+                                </li>
+                                {item.subMenus ? item.subMenus.map((subItem, index) => {
+                                    return <>
+                                        <li id={index}>
+                                            <p className="py-0.5 text-white transition-all duration-300 ease-linear opacity-50 whitespace-nowrap hover:opacity-70">{subItem.title}</p>
+                                        </li>
+                                    </>
+                                })
+                                : ""}
+                            </ul>
+                        </li>
+                    )}
+                    
+                </ul>
+            </nav>
+        </>
+    }
+    
+    return <>
+    {menuToggle ? <Open /> : <Closed />
+        
+    }
+        
     </>
 }
 
-export default SidebarMenu
+export default SidebarMenu2
